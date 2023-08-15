8cd24fa1948b15de32eea76e2528b2173d9959a8
Kenza Ait Salem <keait-sa@e3r7p11.1337.ma>
2023-08-15 15:36:49 +0100

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,11 +3,22 @@
 
 /** struct define a nme and 
  age and owner of dog **/
-typedef struct s_dog
+
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} t_dog;
+};
+
+/**
+ * dog_t - Typedef for dog structure
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
